8003b023929145a2dd47f23c925479c921f8ff9b
bjungbogati <8600091+bjungbogati@users.noreply.github.com>
2025-08-04 00:39:11 +0545

    Added changes

---
+++ adcm.R
@@ -0,0 +1,20 @@
+# *******************************************************************************************************
+# Program Name:                 adcm.R
+# Project:                      TRA-025
+# Purpose:                      Create ADaM Dataset of ADCM
+# Original Author:              Binod Jung Bogati (linkedin.com/in/bjungbogati)
+# Copyright:                    © 2025. Unauthorized distribution or reuse prohibited.
+# Date Created:                 8/04/2025
+# Parameters:                   NA
+#
+# Input:                        cm suppcm dm
+# Output:                       
+# Modifications:
+#   Date        By             Changes
+# **********  *************  *************************************
+#   
+# ********************************************************************************************************/
+
+
+
+
---
+++ excercise.R
@@ -0,0 +1,21 @@
+# R Trainer: Binod Jung Bogati (linkedin.com/in/bjungbogati)
+# copyright: © 2025. Unauthorized distribution or reuse prohibited.
+
+
+
+load(url("https://tinyurl.com/25v8tkrh"))
+
+
+# Exercise 1: Write a program to merge CM and SUPPCM dataset.
+
+
+# Exercise 2: Write a program to merge above data with Treatment groups and Treatment start date (TRTSDT) and Treatment end date (TRTEDT) from DM/ADSL.
+
+
+# Exercise 3: In CM dataset, impute the partial dates in CMSTDTC and CMENDTC and create respective imputation flag.
+
+
+# Exercise 4: Create a PREFL (Prior Flag) and  ONTRTFL ONTRTFL (On Treatment Flag).
+
+
+# Exercise 5: Write a full program to generate ADCM dataset.
